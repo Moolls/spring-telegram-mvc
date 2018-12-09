ff20992cf7253908b7876c705e6f3d033408e2b8
Maxim Levshin <maxim8levshin@gmail.com>
2018-12-09 18:09:07 +0300

    Transfer to default checkstyle

--- src/main/java/ru/moolls/telemvc/TelegramBotApplication.java
+++ src/main/java/ru/moolls/telemvc/TelegramBotApplication.java
@@ -7,10 +7,10 @@ import org.telegram.telegrambots.ApiContextInitializer;
 @SpringBootApplication
 public class TelegramBotApplication {
 
-  public static void main(String[] args) {
-    ApiContextInitializer.init();
-    SpringApplication.run(TelegramBotApplication.class, args);
-  }
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        SpringApplication.run(TelegramBotApplication.class, args);
+    }
 
 
 }
